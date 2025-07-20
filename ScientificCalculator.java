b2053f02c95a8b9470f23bdd9d38623865206900
Mohammed-Alhassni <mohmmedmsoud@gmail.com>
2025-07-20 12:11:09 +0400

    need to fix NoSuchElementException error

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -2,15 +2,30 @@ import java.util.Scanner;
 
 public class ScientificCalculator {
     public static void main(String[] args) {
-        while(true) {
+       boolean isContinued = true;
+        while(isContinued) {
+            double result;
             int userChoice= menuDisplay();
             switch (userChoice){
                 case 1:
-                    double result= add();
+                    result= add();
                     System.out.print("The result is: " + result + "\n");
                     break;
                 case 2:
+                    result= subtract();
+                    System.out.print("The result is: " + result + "\n");
+                    break;
+                case 3:
+                    result= multiply();
+                    System.out.print("The result is: " + result + "\n");
+                    break;
+                case 4:
+                    result= divide();
+                    System.out.print("The result is: " + result + "\n");
+                    break;
+                case 0:
                     System.out.print("Application terminated \n");
+                    isContinued= false;
                     break;
                 default:
                     System.out.println("Wrong choice");
@@ -20,8 +35,17 @@ public class ScientificCalculator {
 
     public static int menuDisplay(){
         Scanner input = new Scanner(System.in);
-        System.out.print("Choose the type of Operation by entering the number: \n 1. Add \n 2. exit \n ");
-        int userChoice = input.nextInt();
+        int userChoice;
+        try{
+            System.out.print("Choose the type of Operation by entering the number: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n enter 0 to exit \n ");
+            userChoice = input.nextInt();
+        } catch (Exception e) {
+            userChoice = -1;
+            System.out.println("Wrong choice");
+            menuDisplay();
+        } finally {
+            input.close();
+        }
         return userChoice;
     }
 
@@ -32,5 +56,32 @@ public class ScientificCalculator {
         System.out.print("Enter the second number: ");
         double num2= numbersReader.nextDouble();
         return num1+num2;
+    }
+
+     public static double subtract() {
+         Scanner numbersReader = new Scanner(System.in);
+         System.out.print("Enter the first number: ");
+         double num1= numbersReader.nextDouble();
+         System.out.print("Enter the second number: ");
+         double num2= numbersReader.nextDouble();
+         return num1-num2;
+     }
+
+    public static double multiply() {
+        Scanner numbersReader = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1= numbersReader.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2= numbersReader.nextDouble();
+        return num1*num2;
+    }
+
+    public static double divide() {
+        Scanner numbersReader = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1= numbersReader.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2= numbersReader.nextDouble();
+        return num1/num2;
     }
 }
