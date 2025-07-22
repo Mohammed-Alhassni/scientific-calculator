1f526399cd75a3ba4f58f28bc71bb6fc54ed029e
mohammed <mohammed@domain.com>
2025-07-22 08:51:22 +0400

    implemented all required methods, stll needs a revision

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -34,6 +34,30 @@ public class ScientificCalculator {
                 case 9:
                     performTanget();
                     break;
+                case 10:
+                    performNaturalLogarithm();
+                    break;
+                case 11:
+                    performLogarithmBase10();
+                    break;
+                case 12:
+                    performAbslout();
+                    break;
+                case 13:
+                    performRoundNumber();
+                    break;
+                case 14:
+                    performCeilingNumber();
+                    break;
+                case 15:
+                    performFloorNumber();
+                    break;
+                case 16:
+                    performFindMin();
+                    break;
+                case 17:
+                    performFindMax();
+                    break;
                 case 0:
                     System.out.print("Application terminated \n");
                     isContinued= false;
@@ -48,7 +72,7 @@ public class ScientificCalculator {
         Scanner input = new Scanner(System.in);
         int userChoice;
         try{
-            System.out.print("Choose the type of Operation by entering the number: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n 5. Calculate Square Root \n 6. Calculate Power \n 7. Calculate Sine \n 8. Calculate Cosine \n 9. Calculate Tanget  \n enter 0 to exit \n ");
+            System.out.print("Choose the type of Operation by entering the number: \n 1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide \n 5. Calculate Square Root \n 6. Calculate Power \n 7. Calculate Sine \n 8. Calculate Cosine \n 9. Calculate Tanget \n 10. Calculate Natural Logarithm  \n 11. Calculate Logarithm Base 10 \n 12. Calculate Absloute Value \n 13. Find Nearest Round  \n 14. Find Ceiling  \n 15. Find Floor \n 16. Find Min \n 17.  Find Max  \n enter 0 to exit \n ");
             userChoice = input.nextInt();
         } catch (Exception e) {
             userChoice = -1;
@@ -253,6 +277,190 @@ public class ScientificCalculator {
             System.out.println("Valid numbers should be entered!");
             result = 0;
             performTanget();
+        }
+        return result;
+    }
+
+    public static double calculateNaturalLogarithm(double num) {
+        double result= Math.log(num);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performNaturalLogarithm(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            double num= numbersReader.nextDouble();
+            if (num>0){
+                result= calculateNaturalLogarithm(num);
+            } else {
+                System.out.println("Positive numbers should be entered!");
+                result = 0;
+                performNaturalLogarithm();
+            }
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performNaturalLogarithm();
+        }
+        return result;
+    }
+
+    public static double calculateLogarithmBase10(double num) {
+        double result= Math.log(num);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performLogarithmBase10(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            double num= numbersReader.nextDouble();
+            if (num>0){
+                result= calculateLogarithmBase10(num);
+            } else {
+                System.out.println("Positive numbers should be entered!");
+                result = 0;
+                performLogarithmBase10();
+            }
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performLogarithmBase10();
+        }
+        return result;
+    }
+
+    public static double abslout(double num) {
+        double result = Math.abs(num);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performAbslout(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            double num= numbersReader.nextDouble();
+            result= abslout(num);
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performAbslout();
+        }
+        return result;
+    }
+
+    public static double roundNumber(double num) {
+        double result = Math.round(num);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performRoundNumber(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            double num= numbersReader.nextDouble();
+            result= roundNumber(num);
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performRoundNumber();
+        }
+        return result;
+    }
+
+    public static double ceilingNumber(double num) {
+        double result = Math.ceil(num);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performCeilingNumber(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            double num= numbersReader.nextDouble();
+            result= ceilingNumber(num);
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performCeilingNumber();
+        }
+        return result;
+    }
+
+    public static double floorNumber(double num) {
+        double result = Math.floor(num);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performFloorNumber(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            double num= numbersReader.nextDouble();
+            result= floorNumber(num);
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performFloorNumber();
+        }
+        return result;
+    }
+
+    public static double findMin(double num1, double num2) {
+        double result = Math.min(num1, num2);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performFindMin(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            double num1 = numbersReader.nextDouble();
+            System.out.print("Enter the second number: ");
+            double num2 = numbersReader.nextDouble();
+            result= findMin(num1, num2);
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performFindMin();
+        }
+        return result;
+    }
+
+    public static double findMax(double num1, double num2) {
+        double result = Math.min(num1, num2);
+        System.out.print("The result is: " + result + "\n");
+        return result;
+    }
+
+    private static double performFindMax(){
+        double result;
+        try {
+            Scanner numbersReader = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            double num1 = numbersReader.nextDouble();
+            System.out.print("Enter the second number: ");
+            double num2 = numbersReader.nextDouble();
+            result= findMax(num1, num2);
+        } catch (Exception e) {
+            System.out.println("Valid numbers should be entered!");
+            result = 0;
+            performFindMax();
         }
         return result;
     }
